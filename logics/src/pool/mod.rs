fdf4b2bf62671e4be271c82db346970f485ae46a
darroze <darroze.ar@gmail.com>
2023-02-08 14:29:34 +0900

    extract pool trait

--- logics/src/pool/mod.rs
+++ logics/src/pool/mod.rs
@@ -1,41 +1,17 @@
-use crate::traits::tokens::collateral_token::PSP22CollateralRef;
+use crate::traits::{pool::*, tokens::collateral_token::PSP22CollateralRef};
 use openbrush::{
-    contracts::psp22::{PSP22Ref, PSP22Error},
+    contracts::psp22::PSP22Ref,
     storage::Mapping,
     traits::{AccountId, Balance, Storage},
 };
-
-#[openbrush::wrapper]
-pub type PoolRef = dyn Pool;
-
-#[openbrush::trait_definition]
-pub trait Pool {
-    #[ink(message)]
-    fn deposit(&self, asset: AccountId, amount: Balance) -> Result<()>;
-    // fn withdraw(&self, asset: AccountId, amount: Balance) -> Result<()>;
-    // fn borrow(&self, asset: AccountId, amount: Balance) -> Result<()>;
-    // fn repay(&self, asset: AccountId, amount: Balance) -> Result<()>;
-}
-
-trait Internal {
-    fn _to_share(&self, asset: &AccountId, amount: Balance) -> Result<Balance>;
-}
 
 pub const STORAGE_KEY: u32 = openbrush::storage_unique_key!(Data);
 
 #[derive(Default, Debug)]
 #[openbrush::upgradeable_storage(STORAGE_KEY)]
 pub struct Data {
-    // assets: Mapping<AccountId, Asset>,
     assets: Mapping<AccountId, AccountId>,
 }
-
-#[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
-#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-pub enum Error {
-    PSP22(PSP22Error)
-}
-type Result<T> = core::result::Result<T, Error>;
 
 impl<T: Storage<Data>> Pool for T {
     default fn deposit(&self, asset: AccountId, amount: Balance) -> Result<()> {
@@ -43,7 +19,8 @@ impl<T: Storage<Data>> Pool for T {
         let depositor = Self::env().caller();
         let pool = Self::env().account_id();
 
-        if let Err(err) = PSP22Ref::transfer_from(&asset, depositor, pool, amount, Vec::<u8>::new()) {
+        if let Err(err) = PSP22Ref::transfer_from(&asset, depositor, pool, amount, Vec::<u8>::new())
+        {
             return Err(Error::PSP22(err));
         }
 
@@ -54,6 +31,10 @@ impl<T: Storage<Data>> Pool for T {
 
         Ok(())
     }
+}
+
+trait Internal {
+    fn _to_share(&self, asset: &AccountId, amount: Balance) -> Result<Balance>;
 }
 
 impl<T: Storage<Data>> Internal for T {
---
+++ logics/src/traits/pool.rs
@@ -0,0 +1,24 @@
+use openbrush::{
+    contracts::psp22::PSP22Error,
+    traits::{AccountId, Balance},
+};
+
+#[openbrush::wrapper]
+pub type PoolRef = dyn Pool;
+
+#[openbrush::trait_definition]
+pub trait Pool {
+    #[ink(message)]
+    fn deposit(&self, asset: AccountId, amount: Balance) -> Result<()>;
+    // fn withdraw(&self, asset: AccountId, amount: Balance) -> Result<()>;
+    // fn borrow(&self, asset: AccountId, amount: Balance) -> Result<()>;
+    // fn repay(&self, asset: AccountId, amount: Balance) -> Result<()>;
+}
+
+#[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum Error {
+    PSP22(PSP22Error),
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
