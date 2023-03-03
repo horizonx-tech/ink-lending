4aab9c443bf695fdb024f9500dd0b26f13e48a78
megared05 <05megared@gmail.com>
2023-03-03 21:42:14 +0900

    add: manager - logic, trait

---
+++ logics/manager/mod.rs
@@ -0,0 +1,88 @@
+use openbrush::traits::{AccountId, Storage};
+
+use crate::traits::manager::*;
+
+pub const STORAGE_KEY: u32 = openbrush::storage_unique_key!(Data);
+
+#[derive(Debug)]
+#[openbrush::upgradeable_storage(STORAGE_KEY)]
+pub struct Data {
+    pub pool_admin: AccountId,
+    pub emergency_admin: AccountId,
+}
+
+pub trait Internal {
+    fn _pool_admin(&self) -> AccountId;
+    fn _emergency_admin(&self) -> AccountId;
+    fn _set_pool_admin(&mut self, address: AccountId) -> Result<()>;
+    fn _set_emergency_admin(&mut self, address: AccountId) -> Result<()>;
+
+    // event emission
+    fn _emit_pool_admin_ownership_transferred_event(&self, previous: AccountId, new: AccountId);
+    fn _emit_emergency_admin_ownership_transferred_event(&self, previous: AccountId, new: AccountId);
+}
+
+impl<T: Storage<Data>> Manager for T {
+    default fn pool_admin(&self) -> AccountId {
+        self._pool_admin()
+    }
+
+    default fn emergency_admin(&self) -> AccountId {
+        self._emergency_admin()
+    }
+
+    default fn set_pool_admin(&mut self, id: AccountId) -> Result<()> {
+        let previous = self._pool_admin();
+        self._set_pool_admin(id)?;
+        self._emit_pool_admin_ownership_transferred_event(previous, id);
+        Ok(())
+    }
+
+    default fn set_emergency_admin(&mut self, id: AccountId) -> Result<()> {
+        let previous = self._emergency_admin();
+        self._set_emergency_admin(id)?;
+        self._emit_emergency_admin_ownership_transferred_event(previous, id);
+        Ok(())
+    }
+}
+
+impl Default for Data {
+    fn default() -> Self {
+        Self {
+            pool_admin: [0u8; 32].into(),
+            emergency_admin: [0u8; 32].into(),
+        }
+    }
+}
+
+impl<T: Storage<Data>> Internal for T {
+    default fn _pool_admin(&self) -> AccountId {
+        self.data().pool_admin
+    }
+
+    fn _emergency_admin(&self) -> AccountId {
+        self.data().emergency_admin
+    }
+
+    fn _set_pool_admin(&mut self, address: AccountId) -> Result<()> {
+        self.data().pool_admin = address;
+        Ok(())
+    }
+
+    fn _set_emergency_admin(&mut self, address: AccountId) -> Result<()> {
+        self.data().emergency_admin = address;
+        Ok(())
+    }
+
+    fn _emit_pool_admin_ownership_transferred_event(
+        &self,
+        _previous: AccountId,
+        _new: AccountId
+    ) {}
+
+    fn _emit_emergency_admin_ownership_transferred_event(
+        &self,
+        _previous: AccountId,
+        _new: AccountId
+    ) {}
+}
---
+++ logics/traits/manager.rs
@@ -0,0 +1,27 @@
+use openbrush::traits::AccountId;
+
+#[openbrush::wrapper]
+pub type ManagerRef = dyn Manager;
+
+#[openbrush::trait_definition]
+pub trait Manager {
+    #[ink(message)]
+    fn pool_admin(&self) -> AccountId;
+
+    #[ink(message)]
+    fn set_pool_admin(&mut self, id: AccountId) -> Result<()>;
+
+    #[ink(message)]
+    fn emergency_admin(&self) -> AccountId;
+
+    #[ink(message)]
+    fn set_emergency_admin(&mut self, id: AccountId) -> Result<()>;
+}
+
+#[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum Error {
+    NewOwnerIsZero,
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
