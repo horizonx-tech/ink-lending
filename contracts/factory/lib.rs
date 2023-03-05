73f6dcb6395e550793ff51b9889e18c4f97c9869
megared05 <05megared@gmail.com>
2023-03-05 15:37:14 +0900

    update: factory - add view functions & offchain test

--- contracts/factory/lib.rs
+++ contracts/factory/lib.rs
@@ -40,6 +40,14 @@ pub mod factory {
         pub fn registry(&self) -> AccountId {
             self.factory.registry
         }
+        #[ink(message)]
+        pub fn pool_code_hash(&self) -> Hash {
+            self.factory.pool_code_hash
+        }
+        #[ink(message)]
+        pub fn shares_code_hash(&self) -> Hash {
+            self.factory.shares_code_hash
+        }
     }
 
     impl Internal for FactoryContract {
@@ -75,6 +83,45 @@ pub mod factory {
             .instantiate();
 
             Ok(pool.to_account_id())
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use ink::env::{
+            test::{
+                self,
+                DefaultAccounts,
+            },
+            DefaultEnvironment,
+        };
+
+        fn default_accounts() -> DefaultAccounts<DefaultEnvironment> {
+            test::default_accounts::<DefaultEnvironment>()
+        }
+        fn set_caller(id: AccountId) {
+            test::set_caller::<DefaultEnvironment>(id);
+        }
+
+        #[ink::test]
+        fn new_works() {
+            let accounts = default_accounts();
+            set_caller(accounts.bob);
+
+            let registry = AccountId::from([0xfa; 32]);
+            let pool_code_hash = [1u8; 32].into();
+            let shares_code_hash = [2u8; 32].into();
+            let contract = FactoryContract::new(
+                registry,
+                pool_code_hash,
+                shares_code_hash,
+                AccountId::from([0x0; 32])
+            );
+
+            assert_eq!(contract.registry(), registry);
+            assert_eq!(contract.pool_code_hash(), pool_code_hash);
+            assert_eq!(contract.shares_code_hash(), shares_code_hash);
         }
     }
 }
