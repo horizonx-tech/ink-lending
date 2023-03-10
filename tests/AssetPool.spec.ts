28baad1a31d2146658c81e4e00e5d2b7fb5101be
megared05 <05megared@gmail.com>
2023-03-10 12:42:40 +0900

    clean: formatting tests/AssetPool.spec

--- tests/AssetPool.spec.ts
+++ tests/AssetPool.spec.ts
@@ -1,9 +1,6 @@
 import { Abi } from '@polkadot/api-contract';
 import { encodeAddress } from '@polkadot/keyring';
-import {
-  deployAssetPool,
-  deployRegistry,
-} from './testContractsHelpers';
+import { deployAssetPool, deployRegistry } from './testContractsHelpers';
 import ASSET_POOL_ABI from '../artifacts/asset_pool.json';
 import SHARES_TOKEN_ABI from '../artifacts/shares_token.json';
 
@@ -11,8 +8,8 @@ const zeroAddress = encodeAddress(
   '0x0000000000000000000000000000000000000000000000000000000000000000',
 );
 
-describe("AssetPool spec", () => {
-  describe("only AssetPool", () => {
+describe('AssetPool spec', () => {
+  describe('only AssetPool', () => {
     const setup = async () => {
       const { api, alice: deployer, bob: notManager } = globalThis.setup;
 
@@ -25,37 +22,44 @@ describe("AssetPool spec", () => {
       const assetPool = await deployAssetPool({
         api,
         signer: deployer,
-        args: [registry.address, zeroAddress, zeroAddress, zeroAddress, null, null],
-      })
+        args: [
+          registry.address,
+          zeroAddress,
+          zeroAddress,
+          zeroAddress,
+          null,
+          null,
+        ],
+      });
 
-      return { deployer, registry, assetPool, notManager }
-    }
+      return { deployer, registry, assetPool, notManager };
+    };
 
-    describe(".set_deposit_paused", () => {
-      it("success", async () => {
-        const { deployer, assetPool } = await setup()
+    describe('.set_deposit_paused', () => {
+      it('success', async () => {
+        const { deployer, assetPool } = await setup();
 
-        expect((await assetPool.query.depositPaused()).value.ok).toBeFalsy
+        expect((await assetPool.query.depositPaused()).value.ok).toBeFalsy;
         await assetPool.withSigner(deployer).tx.setDepositPaused(true);
-        expect((await assetPool.query.depositPaused()).value.ok).toBeTruthy
+        expect((await assetPool.query.depositPaused()).value.ok).toBeTruthy;
         await assetPool.withSigner(deployer).tx.setDepositPaused(false);
-        expect((await assetPool.query.depositPaused()).value.ok).toBeFalsy
-      })
+        expect((await assetPool.query.depositPaused()).value.ok).toBeFalsy;
+      });
 
-      it.skip("fail with notManager", async () => {})
-    })
-    describe(".set_borrow_paused", () => {
-      it("success", async () => {
-        const { deployer, assetPool } = await setup()
+      it.skip('fail with notManager', async () => {});
+    });
+    describe('.set_borrow_paused', () => {
+      it('success', async () => {
+        const { deployer, assetPool } = await setup();
 
-        expect((await assetPool.query.borrowPaused()).value.ok).toBeFalsy
+        expect((await assetPool.query.borrowPaused()).value.ok).toBeFalsy;
         await assetPool.withSigner(deployer).tx.setBorrowPaused(true);
-        expect((await assetPool.query.borrowPaused()).value.ok).toBeTruthy
+        expect((await assetPool.query.borrowPaused()).value.ok).toBeTruthy;
         await assetPool.withSigner(deployer).tx.setBorrowPaused(false);
-        expect((await assetPool.query.borrowPaused()).value.ok).toBeFalsy
-      })
+        expect((await assetPool.query.borrowPaused()).value.ok).toBeFalsy;
+      });
 
-      it.skip("fail with notManager", async () => {})
-    })
-  })
-})
+      it.skip('fail with notManager', async () => {});
+    });
+  });
+});
